5ecdea1890d02670ba0930288248568fb7a69520
rudi97277 <rudi97277@gmail.com>
2025-06-29 11:35:15 +0700

    fix: [route] remove artisan

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => redirect('dashboard'));
 Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::get('/artisan/migrate', function () {
-    if (request()->key === 'vTTAAR0dp93hwNT2h48ZUIBdxE5wIbyH') {
-        Artisan::call('migrate --seed');
-    }
-});
 
 
 Route::middleware([])->group(function () {
